f350f96aa585e73cdafbf60171767bb69ab247e5
garlic0000 <garlic0000@163.com>
2024-11-02 20:18:50 +0800

    Update save_croped_image.py

--- save_croped_image.py
+++ save_croped_image.py
@@ -120,8 +120,6 @@ def crop_images(opt):
                     # 关于casme_026_0101这部分 右边多裁剪了一部分 在右边增加一些
                     if v_name == "casme_026_0101":
                         face_right = face_right + 2
-                        print("face_right")
-                        print(face_right)
                     face = image[face_top:face_bottom, face_left:face_right]  # 裁剪人脸区域
                     # 不调整尺寸
                     # face = cv2.resize(face, (128, 128))  # 调整尺寸为 128x128
