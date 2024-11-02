feeadd74dbc609e5581cf8527bb8405503d6629f
garlic0000 <garlic0000@163.com>
2024-11-02 19:00:02 +0800

    Update save_croped_image.py

--- save_croped_image.py
+++ save_croped_image.py
@@ -116,6 +116,8 @@ def crop_images(opt):
                     elif v_name == "casme_026_0101":
                         face_right = face_right + 2
                         print(face_right)
+                    print(face_left)
+                    print(face_right)
                     face = image[face_top:face_bottom, face_left:face_right]  # 裁剪人脸区域
                     # 不调整尺寸
                     # face = cv2.resize(face, (128, 128))  # 调整尺寸为 128x128
